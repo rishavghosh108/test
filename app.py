b0911bee726f29628687f7cdc9355ed8f3d7e10c
Rishav Ghosh <rishav@reddensoft.com>
2025-08-03 17:16:28 +0530

    tsds

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def health():
     # just for test
     # test rebase
-    return "Hello Rishav, the server is up!"
+    return "Hello Rishav, the server is up! lsjjos"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050)
